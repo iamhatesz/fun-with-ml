01fa9df97a2a56e9b525c1c6d93be5a7133c7d79
Tomasz Wrona <tomasz@wrona.me>
2023-03-12 14:52:07 +0100

    Added FashionMNISTDataModule

--- gan/data.py
+++ gan/data.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from typing import Type
 
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split, Dataset
+from torchvision.datasets import MNIST, FashionMNIST
 from torchvision.transforms import transforms
 
 
-class MNISTDataModule(LightningDataModule):
-    def __init__(self, data_dir: Path, batch_size: int, num_workers: int = 1):
+class _MNISTLikeDataModule(LightningDataModule):
+    def __init__(self, variant: Type[MNIST], data_dir: Path, batch_size: int, num_workers: int = 1):
         super().__init__()
         self._data_dir = data_dir
         self._batch_size = batch_size
@@ -15,20 +16,21 @@ class MNISTDataModule(LightningDataModule):
 
         self._to_tensor = transforms.ToTensor()
 
+        self._dataset_cls = variant
         self._mnist_train = self._mnist_val = self._mnist_test = None
 
     def prepare_data(self) -> None:
-        MNIST(str(self._data_dir), train=True, download=True)
-        MNIST(str(self._data_dir), train=False, download=True)
+        self._dataset_cls(str(self._data_dir), train=True, download=True)
+        self._dataset_cls(str(self._data_dir), train=False, download=True)
 
     def setup(self, stage: str) -> None:
         if stage == "fit":
-            mnist_full = MNIST(
+            mnist_full = self._dataset_cls(
                 str(self._data_dir), train=True, transform=self._to_tensor
             )
             self._mnist_train, self._mnist_val = random_split(mnist_full, [55000, 5000])
         if stage == "test":
-            self._mnist_test = MNIST(
+            self._mnist_test = self._dataset_cls(
                 str(self._data_dir), train=False, transform=self._to_tensor
             )
 
@@ -50,3 +52,13 @@ class MNISTDataModule(LightningDataModule):
         return DataLoader(
             self._mnist_test, batch_size=self._batch_size, num_workers=self._num_workers
         )
+
+
+class MNISTDataModule(_MNISTLikeDataModule):
+    def __init__(self, data_dir: Path, batch_size: int, num_workers: int = 1):
+        super().__init__(MNIST, data_dir, batch_size, num_workers)
+
+
+class FashionMNISTDataModule(_MNISTLikeDataModule):
+    def __init__(self, data_dir: Path, batch_size: int, num_workers: int = 1):
+        super().__init__(FashionMNIST, data_dir, batch_size, num_workers)
